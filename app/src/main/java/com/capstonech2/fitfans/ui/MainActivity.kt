e64e8b5df0a8d19e4cbc5b69dd8fe797427a2add
GerrysatriaH <gerrysatria10@gmail.com>
2023-12-20 01:14:31 +0700

    minor changes

--- app/src/main/java/com/capstonech2/fitfans/ui/MainActivity.kt
+++ app/src/main/java/com/capstonech2/fitfans/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.capstonech2.fitfans.ui
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,11 +13,25 @@ import androidx.navigation.ui.setupWithNavController
 import com.capstonech2.fitfans.R
 import com.capstonech2.fitfans.databinding.ActivityMainBinding
 import com.capstonech2.fitfans.ui.camera.CameraActivity
+import com.capstonech2.fitfans.utils.showToast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
+
+    // New
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                showToast(this, getString(R.string.notification_granted))
+            } else {
+                showToast(this, getString(R.string.notification_not_granted))
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,6 +39,14 @@ class MainActivity : AppCompatActivity(){
         supportActionBar?.hide()
 
         navigationConfig()
+
+        // New
+        if (Build.VERSION.SDK_INT >= 33){
+            requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
         binding.buttonCamera.setOnClickListener { intentCamera() }
     }
 
