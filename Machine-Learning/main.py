e9ffb05c4b621a9faee5aa1650272061858dc3b5
Rega Anton Giapierro <rega22052002@gmail.com>
2023-12-20 20:51:07 +0700

    update code and dataset

--- Machine-Learning/main.py
+++ Machine-Learning/main.py
@@ -1,15 +1,19 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
-# Path dataset
-train_path = 'training'
-val_path = 'validasi'
+# Path ke folder train dan validasi
+train_dir = 'dataset/training'
+validation_dir = 'dataset/testing'
 
-# Preprocess data and augmentations
+# Parameter untuk model
+input_shape = (224, 224, 3)
+num_classes = 10
+epochs = 30
+batch_size = 32
+
+# Preprocessing data
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -17,63 +21,66 @@ train_datagen = ImageDataGenerator(
     horizontal_flip=True
 )
 
-val_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
 
-# Load dataset
 train_generator = train_datagen.flow_from_directory(
-    train_path,
-    target_size=(224, 224),
-    batch_size=32,
+    train_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
     class_mode='categorical'
 )
 
-val_generator = val_datagen.flow_from_directory(
-    val_path,
-    target_size=(224, 224),
-    batch_size=32,
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
     class_mode='categorical'
 )
 
-# Define the model
-model = Sequential()
+# Membangun model CNN
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(num_classes, activation='softmax'))
 
-model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(10, activation='softmax'))
-
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.001), 
-              loss='categorical_crossentropy', 
+# Compile model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# Train the model
+# Training model
 history = model.fit(
     train_generator,
-    steps_per_epoch=train_generator.samples // train_generator.batch_size,
-    epochs=10,
-    validation_data=val_generator,
-    validation_steps=val_generator.samples // val_generator.batch_size
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size
 )
 
-# Plot training history
+# Menyimpan model terakhir
+model.save('Gym_Tools_Multi.h5')
+
+# Plot akurasi dan loss
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
 plt.plot(history.history['accuracy'], label='Training Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
-plt.show()
 
-# Menyimpan model terakhir
-model.save('Gym_Tools_Multi.h5')
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
